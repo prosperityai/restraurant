120c5f5376b350f1272a47102f73e1ad21546b2b
prosperityai <prosperpaul@ymail.com>
2018-05-09 20:46:38 +0300

    cred js

---
+++ cred.js
@@ -0,0 +1,14 @@
+module.exports = {
+    AT:{
+        apiKey: 'f7d89c7ec8b6f93fbf9f5ed60159473f2dce329976ecaa302f9aca154f4e7fb4',
+        username: 'prosperityai',
+        format: 'json'
+    },
+    pusher:{
+       appId: '523024',
+       key: '602e3e50a98bdf42160f',
+       secret: "6d099e28586aad7bfb09",
+       cluster: "ap2",
+       encrypted: true
+    }
+   }
