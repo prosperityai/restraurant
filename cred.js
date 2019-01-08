afe213e19b222ce419311147cbf2d56712a058c4
Prosperity Paul <prosperpaul@ymail.com>
2019-01-08 15:39:22 +0300

    Update cred.js

--- cred.js
+++ cred.js
@@ -1,13 +1,13 @@
 module.exports = {
     AT:{
-        apiKey: 'f7d89c7ec8b6f93fbf9f5ed60159473f2dce329976ecaa302f9aca154f4e7fb4',
+        apiKey: 'YourAPI-Key',
         username: 'prosperityai',
         format: 'json'
     },
     pusher:{
-       appId: '523024',
-       key: '602e3e50a98bdf42160f',
-       secret: "6d099e28586aad7bfb09",
+       appId: 'Your APId',
+       key: 'Key',
+       secret: "Your Secrete",
        cluster: "ap2",
        encrypted: true
     }
